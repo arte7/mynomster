1ccb4b5946624326dc7166f0ab8f486d1f98414e
Pia Bregulla <pia.bregulla@xing.com>
2017-03-13 13:32:31 +0100

    made restrictions to editAND UDATE AN D DESTROY ENTRIES

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], :per_page =>5)
@@ -20,16 +20,28 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if  @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+    if @place.user !=current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
+
     @place.update_attribute(place_params)
     redirect_to root_path
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: forbidden
+    end
+
     @place.destroy
     redirect_to root_path
   end
