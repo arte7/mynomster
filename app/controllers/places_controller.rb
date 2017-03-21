f2039715a8bcace454fc695cd908126662e892c7
Pia Bregulla <pia.bregulla@xing.com>
2017-03-21 16:15:42 +0100

    found typo in update section of places_controllrt

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -37,7 +37,7 @@ class PlacesController < ApplicationController
       return render text: 'Not Allowed', status: :forbidden
     end
 
-    @place.update_attribute(place_params)
+    @place.update_attributes(place_params)
     if @place.valid?
       redirect_to root_path
     else
