40d24934e23bd3ec6c2c5d28263219cad837e806
Pia Bregulla <pia.bregulla@xing.com>
2017-03-21 09:03:41 +0100

    found typos

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -26,7 +26,7 @@ class PlacesController < ApplicationController
   def edit
     @place = Place.find(params[:id])
 
-    if  @place.user != current_user
+    if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
   end
